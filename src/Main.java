2b850825a03cccf64736f62893e0d7be4f503be1
jazzyfact95@gmail.com <jazzyfact95@gmail.com>
2021-10-28 00:29:26 +0900

    스트림 테스트

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
 
@@ -8,5 +10,7 @@ public class Main {
         System.out.println(list.toString());
         list.stream().forEach(str -> str = "ttt");
         System.out.println(list.toString());
+
+        IntStream intStream = Stream.iterate(0, n -> n+2).limit(3);
     }
 }
