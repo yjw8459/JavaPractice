36aa4a1301fd22bd11de1b69a584d5e296539d6a
yjw8459 <ryu01410@gmail.com>
2021-10-30 22:40:29 +0900

    스트림 테스트

--- src/ExcelTest.java
+++ src/ExcelTest.java
@@ -1,38 +1,58 @@
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
 import java.io.FileInputStream;
 
 public class ExcelTest {
 
-    public void excelParsing(){
+    public static void readFile(){
         try {
-            FileInputStream file = new FileInputStream("D:/tmp/upload/right_excel/test.xlsx");
-            XSSFWorkbook workbook = new XSSFWorkbook(file);
-            int rowindex=0;
-            int columnindex=0; //시트 수 (첫번째에만 존재하므로 0을 준다) //만약 각 시트를 읽기위해서는 FOR문을 한번더 돌려준다
-            XSSFSheet sheet=workbook.getSheetAt(0); //행의 수
-            int rows=sheet.getPhysicalNumberOfRows();
-            for(rowindex=0;rowindex<rows;rowindex++){ //행을읽는다
-                 XSSFRow row=sheet.getRow(rowindex);
-                 if(row !=null){ //셀의 수
-                 int cells=row.getPhysicalNumberOfCells();
-                 for(columnindex=0; columnindex<=cells; columnindex++){ //셀값을 읽는다
-                     XSSFCell cell=row.getCell(columnindex);
-                     String value=""; //셀이 빈값일경우를 위한 널체크
-                     if(cell==null){ continue; }
-                     else{ //타입별로 내용 읽기
-                         switch (cell.getCellType()){
-                             case XSSFCell.CELL_TYPE_FORMULA: value=cell.getCellFormula(); break;
-                             case XSSFCell.CELL_TYPE_NUMERIC: value=cell.getNumericCellValue()+""; break;
-                             case XSSFCell.CELL_TYPE_STRING: value=cell.getStringCellValue()+""; break;
-                             case XSSFCell.CELL_TYPE_BLANK: value=cell.getBooleanCellValue()+""; break;
-                             case XSSFCell.CELL_TYPE_ERROR: value=cell.getErrorCellValue()+""; break;
-                         }
-                     }
-                     System.out.println(rowindex+"번 행 : "+columnindex+"번 열 값은: "+value);
-                 }
-                 }
-            }
-        }
-        catch(Exception e) { e.printStackTrace(); }
+            String filePath = "/Users/yjw8459/Desktop/Project/Dream /excelTest.xlsx" ;
+            File file = new File(filePath);
+            System.out.println(file.getName());
 
+
+            //엑셀 파일 로딩
+            FileInputStream fis = new FileInputStream(file);
+            //POIFSFileSystem fileSystem = new POIFSFileSystem(fis);
+            XSSFWorkbook workBook = new XSSFWorkbook(fis);
+            System.out.println(fis);
+
+            int sheeetNum = workBook.getNumberOfSheets();   //시트 갯수 얻기
+            System.out.println(sheeetNum);
+
+            for (int loop = 0; loop < sheeetNum; loop++){
+                XSSFSheet sheet = workBook.getSheetAt(loop);   //i 번째 sheet 부터 작업 시작
+                int rows = sheet.getPhysicalNumberOfRows();//행 갯수 출력
+                for ( int rowNum = 0; rowNum < rows; rowNum ++ ){
+                    XSSFRow row = sheet.getRow(rowNum);
+                    if ( row != null ){
+                        int cells = row.getPhysicalNumberOfCells();
+                        for ( int cellNum = 0; cellNum < cells; cellNum++ ){
+                            XSSFCell cell = row.getCell(cellNum);
+
+                        }
+                    }
+                }
+
+            }
+
+        }
+        catch (Exception e){ e.printStackTrace(); }
     }
+
+
+//    public static void excelParsing(){
+//        Workbook wb = new HSSFWorkbook();
+//        Sheet sheet = wb.createSheet("테스트1");
+//        Row row = null;
+//        Cell cell = null;
+//        int rowNo = 0;
+//
+//
+//    }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,10 @@ import java.util.stream.Stream;
 public class Main {
 
     public static void main(String[] args) {
+        ExcelTest.readFile();
+    }
+
+    public static void streamTest(){
         List<String> list = Arrays.asList("test","test1");
         System.out.println(list.toString());
         list.stream().forEach(str -> str = "ttt");
@@ -14,7 +18,5 @@ public class Main {
         List<Integer> intList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         List<Integer> collect = intList.stream().filter(num -> num % 2 == 0).collect(Collectors.toList());
         System.out.println(collect.toString());
-
-
     }
 }
