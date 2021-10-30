a8929cafd49e13c6969ae064c2caa9d4ee72071e
yjw8459 <ryu01410@gmail.com>
2021-10-30 23:09:58 +0900

    엑셀 테스트

--- src/ExcelTest.java
+++ src/ExcelTest.java
@@ -6,6 +6,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class ExcelTest {
 
@@ -18,29 +19,34 @@ public class ExcelTest {
 
             //엑셀 파일 로딩
             FileInputStream fis = new FileInputStream(file);
-            //POIFSFileSystem fileSystem = new POIFSFileSystem(fis);
             XSSFWorkbook workBook = new XSSFWorkbook(fis);
             System.out.println(fis);
 
-            int sheeetNum = workBook.getNumberOfSheets();   //시트 갯수 얻기
+            //XSSFSheet xSheet = workBook.createSheet("test시트");        //시트 생성
+            int sheeetNum = workBook.getNumberOfSheets();                //시트 갯수 얻기
             System.out.println(sheeetNum);
 
+            /**
+             *
+             */
             for (int loop = 0; loop < sheeetNum; loop++){
                 XSSFSheet sheet = workBook.getSheetAt(loop);   //i 번째 sheet 부터 작업 시작
                 int rows = sheet.getPhysicalNumberOfRows();//행 갯수 출력
+                System.out.println(rows);
                 for ( int rowNum = 0; rowNum < rows; rowNum ++ ){
                     XSSFRow row = sheet.getRow(rowNum);
                     if ( row != null ){
                         int cells = row.getPhysicalNumberOfCells();
                         for ( int cellNum = 0; cellNum < cells; cellNum++ ){
+                            System.out.println(cells);
                             XSSFCell cell = row.getCell(cellNum);
-
                         }
                     }
                 }
-
             }
 
+            FileOutputStream fos = new FileOutputStream(file);  //FileOutStream 
+            workBook.write(fos);                                //작성 내용 저장
         }
         catch (Exception e){ e.printStackTrace(); }
     }
