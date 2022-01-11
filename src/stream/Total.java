5238809d386a2857a56ccab64ec65accc8ff8ca0
yjw8459 <ryu01410@gmail.com>
2022-01-11 23:01:20 +0900

    Stream 정리

--- src/stream/Total.java
+++ src/stream/Total.java
@@ -15,7 +15,7 @@ import java.util.stream.IntStream;
 public class Total {
 
     public static void main(String[] args) {
-        map();
+        optional();
     }
 
     static class Cat{
@@ -69,6 +69,7 @@ public class Total {
      * 집계 함수
      */
     public void totalFunction(){
+
         //sum, count는 정수 타입 반환
         long sum = Arrays.stream( new int[] {1, 2, 3, 4, 5, 6} ).filter(n -> n % 2 == 0).sum();
         long count = Arrays.stream( new int[] {1, 2, 3, 4, 5, 6} ).filter(n -> n % 2 == 0).count();
@@ -77,6 +78,7 @@ public class Total {
         /**
          * optional 객체 반환
          */
+
         //OptionalDouble
         double avg = Arrays.stream( new int[] {1, 2, 3, 4, 5, 6} ).filter(n -> n % 2 == 0).average().getAsDouble();
 
@@ -84,12 +86,14 @@ public class Total {
         long max = Arrays.stream( new int[] {1, 2, 3, 4, 5, 6} ).filter(n -> n % 2 == 0).max().getAsInt();
         long min = Arrays.stream( new int[] {1, 2, 3, 4, 5, 6} ).filter(n -> n % 2 == 0).min().getAsInt();
         int first = Arrays.stream( new int[] {1, 2, 3, 4, 5, 6} ).filter(n -> n % 2 == 0).findFirst().getAsInt();
+
     }
 
     /**
      * optional
      */
-    public void optional(){
+    public static void optional(){
+
         List<Integer> list = new ArrayList<>();
 
         /**
@@ -103,12 +107,13 @@ public class Total {
         /**
          * 방법 2: orElse를 통해서 값이 없을 경우 0.0으로 계산하도록 예외처리
          */
-        double avg = list.stream().mapToInt(Integer::intValue).average().orElse(0.0);
+        System.out.println(list.stream().mapToInt(Integer::intValue).average().orElse(0.0));
 
         /**
          * 방법 3 : ifPresent를 통해서 값이 있을 경우만 계산하도록 예외처리
          */
         list.stream().mapToInt(Integer::intValue).average().ifPresent(a -> System.out.println(a));
+
     }
 
     public void reduce(){
@@ -145,5 +150,6 @@ public class Total {
 
 
     }
+
 
 }
